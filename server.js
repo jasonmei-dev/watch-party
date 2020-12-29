6bc08b5469f1c80cc2a3782d296f65aaa78e97b9
jmei403 <jason.mei403@gmail.com>
2020-12-29 17:51:51 -0500

    add env port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
