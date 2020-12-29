b6d469da19d5072dd79ada115003f5dd72f0844e
jmei403 <jason.mei403@gmail.com>
2020-12-29 17:45:05 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.get('/random', (req, res) => {
+  let rand = Math.random();
+  res.json({
+    data: rand
+  })
+})
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/index.html');
+// });
+
+// app.get('/style.css', (req, res) => {
+//   res.sendFile(__dirname + '/style.css');
+// })
+
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+  socket.on('chat message', msg => {
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
+
+});
+
+http.listen(port, () => {
+  console.log(`Listening on port:${port}`);
+})
